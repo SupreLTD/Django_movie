b28330c4352636866e6c2a86aa105e80bb89fe3f
Vanya <balanaaarrrr@gmail.com>
2023-01-05 12:29:01 +0300

    fix_models

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-0d10a!0d7963rg+@2y)1=x+5mnk9achjbtb8$2ej254ybjsp9!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
