119e80e13ea9d5888f2f9c485d8c4940e15a7706
Vanya <balanaaarrrr@gmail.com>
2023-01-05 19:17:12 +0300

    Refactor_views

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -67,6 +68,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
 
     class Meta:
         verbose_name = 'Фильм'
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Film list view"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movies.html'
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
+
+class MovieDetailView(DetailView):
+    """Full film description"""
+    model = Movie
+    slug_field = 'url'
